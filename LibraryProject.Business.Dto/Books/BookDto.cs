e05c76e2bdf72f4114e7c8b514a7f5fe80fb268f
Steven <steven.bouche@outlook.com>
2022-02-15 09:21:29 +0100

    fucking project

---
+++ LibraryProject.Business.Dto/Books/BookDto.cs
@@ -0,0 +1,29 @@
+﻿using LibraryProject.Business.Dto.Genres;
+using Newtonsoft.Json;
+
+namespace LibraryProject.Business.Dto.Books
+{
+    public class BookDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty("author")]
+        public string Author { get; set; } = string.Empty;
+
+        [JsonProperty("price")]
+        public double Price { get; set; }
+
+        [JsonProperty("books")]
+        public IList<GenreDto> Genres { get; set; } = new List<GenreDto>();
+
+        [JsonProperty("createdAt")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
---
+++ LibraryProject.Business.Dto/Genres/GenreDto.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace LibraryProject.Business.Dto.Genres
+{
+    public class GenreDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonProperty("createdAt")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
