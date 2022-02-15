d294712f901a96cf8304b25ae45e00d973274f7f
Armand Prévot <armand-prevot@hotmail.fr>
2022-02-15 08:41:58 +0100

    fix csproj

---
+++ LibraryProject.API/Controllers/Common/BookController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryProject.API.Controllers.Common
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : LibraryBaseController
+    {
+        public BookController(ILogger<LibraryBaseController> logger) : base(logger)
+        {
+        }
+    }
+}
