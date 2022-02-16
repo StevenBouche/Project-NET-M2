c9b7fd3d56bd37c360487d9b5a671e8436894d9f
Armand prevot <armand.prevot@etu.unice.fr>
2022-02-16 17:15:16 +0100

    dto for PUT

--- LibraryProject.Business/BookBusiness/BookService.cs
+++ LibraryProject.Business/BookBusiness/BookService.cs
@@ -4,11 +4,6 @@ using LibraryProject.Business.Exceptions;
 using LibraryProject.Domain.Entities;
 using LibraryProject.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibraryProject.Business.BookBusiness
 {
