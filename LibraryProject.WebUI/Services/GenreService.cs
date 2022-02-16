36327a4905a45b800e9dc35170575fffb4b85dbb
Steven <steven.bouche@outlook.com>
2022-02-16 13:54:17 +0100

    Genre service

---
+++ LibraryProject.WebUI/Services/GenreService.cs
@@ -0,0 +1,19 @@
+﻿using LibraryProject.Business.Dto.Genres;
+
+namespace LibraryProject.WebUI.Services
+{
+    public class GenreService
+    {
+        private readonly List<GenreDto> Genres = new()
+        {
+            new GenreDto() { Id = 1, Name = "Genre1", CreatedAt = DateTimeOffset.UtcNow, UpdatedAt = DateTimeOffset.UtcNow },
+            new GenreDto() { Id = 2, Name = "Genre2", CreatedAt = DateTimeOffset.UtcNow, UpdatedAt = DateTimeOffset.UtcNow },
+            new GenreDto() { Id = 3, Name = "Genre3", CreatedAt = DateTimeOffset.UtcNow, UpdatedAt = DateTimeOffset.UtcNow }
+        };
+
+        public List<GenreDto> GetBooks()
+        {
+            return Genres;
+        }
+    }
+}
