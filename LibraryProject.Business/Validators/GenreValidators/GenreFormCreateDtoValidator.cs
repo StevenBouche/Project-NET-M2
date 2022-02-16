9bf7cce1c3982fc981c378cc9b04018bffafe357
Camille PERES <camille.peres@free.fr>
2022-02-16 11:40:02 +0100

    Changes on class

---
+++ LibraryProject.Business/Validators/GenreValidators/GenreFormCreateDtoValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using LibraryProject.Business.Dto.Genres;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Business.Validators.GenreValidators
+{
+    public class GenreFormCreateDtoValidator : AbstractValidator<GenreFormCreateDto>
+    {
+        public GenreFormCreateDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty()
+                .WithMessage("{PropertyName} should be not empty")
+                .Must(IsValidName).WithMessage("{PropertyName} should be only letters.");
+        }
+
+        private bool IsValidName(string name)
+        {
+            return name.All(Char.IsLetter);
+        }
+    }
+
+   
+}
