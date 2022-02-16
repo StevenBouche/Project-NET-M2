5a5c6c7d3873cb9bd3e170d0c667db3b2a362d9d
Camille PERES <camille.peres@free.fr>
2022-02-16 16:36:47 +0100

    WebSocketClient

---
+++ LibraryProject.WebSocket/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LibraryProject.Business.Dto.Books;
+using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello, World!");
+
+HubConnection connection;
+
+connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:8080/LibraryHub")
+                .Build();
+
+connection.Closed += async (error) =>
+{
+    await Task.Delay(new Random().Next(0, 5) * 1000);
+    await connection.StartAsync();
+};
+
+connection.On<BookDetailsDto>("OnCreatedBook", book =>
+{
+
+    string output = JsonConvert.SerializeObject(book);
+    Console.WriteLine($"Nouveau {output} cree");
+});
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
