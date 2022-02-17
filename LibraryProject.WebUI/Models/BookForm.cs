12e255dd6888e86c1736c11cfbc8f7ffafad15ce
Steven <steven.bouche@outlook.com>
2022-02-17 15:56:32 +0100

    update webui

---
+++ LibraryProject.WebUI/Models/BookForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace LibraryProject.WebUI.Models
+{
+    public class BookForm
+    {
+        public int IdBook { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int Price { get; set; }
+        public List<int> GenresIds { get; set; } = new();
+    }
+}
